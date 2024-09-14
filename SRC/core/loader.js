fd3db1a114ac9cae09ca88920743b475ff8aba25
ya_maou <yamaou0210@gmail.com>
2024-09-14 19:49:11 +0800

    Updated and change the acount

--- SRC/core/loader.js
+++ SRC/core/loader.js
@@ -1,29 +1,67 @@
-import { REST,Routes } from'discord.js'
+import { REST,Routes, Collection } from'discord.js'
 import fg from 'fast-glob'
+import { useAppStore } from '@/store/app'
 
 const updateSlashCommands = async(commands) => {
     const rest = new REST().setToken(process.env.TOKEN)
-    const result = await rest.put(
-        Routes.applicationGuildCommands(
-            process.env.APPLICATION_ID,
-            '986515567328505896',
-        ),
-        {
-            body:commands,
-        },
-    )
 
-    console.log(result)
-}
+    const guildIds = [
+        '986515567328505896',
+        '1223820038885085281',
+    ];
+
+    for (const guildId of guildIds) {
+        try {
+            const result = await rest.put(
+                Routes.applicationGuildCommands(
+                    process.env.APPLICATION_ID,
+                    guildId,
+                ),
+                { body: commands },
+            );
+            console.log(`Commands updated for guild: ${guildId}`, result);
+        } catch (error) {
+            console.error(`Failed to update commands for guild: ${guildId}`, error);
+        }
+    }
+};
 
 export const loadCommands = async() => {
+    const appStore = useAppStore()
     const commands = []
+    const actions = new Collection()
     const files = await fg('./src/commands/**/index.js')
     for(const file of files){
     const cmd = await import(file)
     console.log(cmd.command)
     commands.push(cmd.command)
+    actions.set(cmd.command.name, cmd.action)
     }
     
     await updateSlashCommands(commands)
+    appStore.commandsActionMap = actions
+
+    console.log(appStore.commandsActionMap)
+}
+
+export const loadEvents = async() => {
+    const appStore = useAppStore()
+    const client = appStore.client
+    const files = await fg('./src/events/**/index.js')
+    for(const file of files){
+        const eventFile = await import(file)
+
+        if(eventFile.event.once){
+        client.once(
+            eventFile.event.name,
+            eventFile.action
+            )
+        }
+        else {
+            client.on(
+                eventFile.event.name,
+                eventFile.action
+            )
+        }
+    }
 }
